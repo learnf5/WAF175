04510a2dd2145aabfc33c8c2cf3218308fb9262a
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-05-26 14:30:08 -0400

    Create 20-WAF175-C3A01-BADOS.sh

---
+++ 20-WAF175-C3A01-BADOS.sh
@@ -0,0 +1,16 @@
+# set scf name
+SCF=rdp.scf
+
+# download config from GitHub, copy to bigip1 and load/merge onto bigip1
+curl --silent https://raw.githubusercontent.com/learnf5/waf/main/scf/$SCF --output /tmp/$SCF
+sudo scp /tmp/$SCF 192.168.1.31:/var/local/scf
+sudo ssh 192.168.1.31 tmsh load sys config merge file $SCF
+
+# download attacks and iRules, move/copy files to the Lab_Files folder and clean up
+cd /tmp
+git clone https://github.com/learnf5/waf.git attacks
+git clone https://github.com/learnf5/waf.git iRules
+mv /tmp/attacks/attacks /home/student/Desktop/Lab_Files
+cp --recursive /tmp/iRules/iRules/. /home/student/Desktop/Lab_Files
+rm --recursive --force /tmp/attacks /tmp/iRules /home/student/Desktop/Lab_Files/harvest_readme.md 
+chmod +x /home/student/Desktop/Lab_Files/*.sh
