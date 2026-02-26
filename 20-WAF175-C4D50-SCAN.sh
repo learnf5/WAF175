f6258c44b21dfbfc5d2002bd3de1a55a5486f453
F5 Education Services <99698691+learnf5@users.noreply.github.com>
2026-02-26 09:53:53 -0500

    Add command to download juice-shop-vulns.xml
    Added command to download juice-shop-vulns.xml to Lab_Files.

--- 20-WAF175-C4D50-SCAN.sh
+++ 20-WAF175-C4D50-SCAN.sh
@@ -5,3 +5,6 @@ SCF=base.scf
 curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/scf/$SCF --output /tmp/$SCF
 sudo scp /tmp/$SCF 192.168.1.31:/var/local/scf
 sudo ssh 192.168.1.31 tmsh load sys config merge file $SCF
+
+# make a file available to the student
+curl https://raw.githubusercontent.com/learnf5/WAF175/refs/heads/main/juice-shop-vulns.xml --remote-name --output-dir ~/Desktop/Lab_Files --create-dirs --silent
